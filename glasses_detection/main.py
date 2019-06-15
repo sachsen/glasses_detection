7e3d843fa6e9e4e8c323afbbf3e310ef7cf7c59d
HK-OU <u558195i@ecs.osaka-u.ac.jp>
2019-06-15 18:20:19 +0900

    めがね検出実装
    閾値 GLASSES_THRESHOLD の値を調整しないといけない。

--- glasses_detection/main.py
+++ glasses_detection/main.py
@@ -9,25 +9,47 @@ eye_cascade=cv2.CascadeClassifier(HAAR_FILE2)
 capture = cv2.VideoCapture(0)
 
 
-while(True):
-    ret, frame = capture.read()
-    img_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face = cascade.detectMultiScale(img_g)
-    for (x, y, w, h) in face:
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
-        img_eye_gray = img_g[y:y + h, x:x + w]
-        img_eye = frame[y:y + h, x:x + w]
-        eyes = eye_cascade.detectMultiScale(img_eye_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(img_eye, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 1)
+def main():
+    while(True):
+        ret, frame = capture.read()
+        img_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face = cascade.detectMultiScale(img_g)
+        for (x, y, w, h) in face:
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+            img_eye_gray = img_g[y:y + h, x:x + w]
+            img_eye = frame[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(img_eye_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(img_eye, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 1)
+            
+            # 検出した目が2個以上なら
+            # TODO: 3個以上の目を検出したときに、その中から2個を選ぶ処理を追加する
+            if len(eyes) >= 2:
+                if detectGlasses(img_eye_gray, (eyes[0][0] + eyes[0][2]/2, eyes[0][1] + eyes[0][3]/2), (eyes[1][0] + eyes[1][2]/2, eyes[1][1] + eyes[1][3]/2)):
+                    cv2.putText(img_eye, "GLASSES", (0, h), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+                else:
+                    cv2.putText(img_eye, "NOT GLASSES", (0, h), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
 
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1000) == 27:
-        break
 
-capture.release()
-cv2.destroyAllWindows()
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1000) == 27:
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+def clip(x, min, max):
+    """
+    xをmin以上max以下の値にする。
+    max < min なら min が返る。
+    """
+    if x <= min:
+        return min
+    if x >= max:
+        return max
+    return x
 
 
 def detectGlasses(img, eye1Pos, eye2Pos):
@@ -50,7 +72,11 @@ def detectGlasses(img, eye1Pos, eye2Pos):
     ret, img_2 = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
 
     # 目の間周辺の画像を切り出し
-    img_betweenEyes = img_2[eyeCenter[1] - eyeDistance/4 : eyeCenter[1] + eyeDistance/4, eyeCenter[0] - eyeDistance/4 : eyeCenter[0] + eyeDistance/4]
+    x2 = clip(int(eyeCenter[0] + eyeDistance/4), 0, img.shape[1])
+    x1 = clip(int(eyeCenter[0] - eyeDistance/4), 0, img.shape[1])
+    y1 = clip(int(eyeCenter[1] - eyeDistance/4), 0, img.shape[0])
+    y2 = clip(int(eyeCenter[1] + eyeDistance/4), 0, img.shape[0])
+    img_betweenEyes = img_2[y1 : y2, x1 : x2]
 
     # 平均の明るさを計算
     average = np.mean(img_betweenEyes)
@@ -59,3 +85,6 @@ def detectGlasses(img, eye1Pos, eye2Pos):
         return True
     else:
         return False
+
+
+main()
